00ffce4f5ed47508f29dfded545b66107d5cb290
angelyangg <57456579+angelyangg@users.noreply.github.com>
2020-06-22 21:24:05 -0400

    fdhsfhs

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+console.log('Connected');
+
+var myVar;
+
+
+function myFunction() {
+  myVar = setTimeout(showPage, 2000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
